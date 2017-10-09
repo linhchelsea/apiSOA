b6f61094d152c0acd9a5398b2a75a98a4b485aab
thanhthuan.bk95 <thanhthuan.bk95@gmail.com>
2017-10-09 12:52:57 +0700

    deploy on heroku

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_0fc84c1c7dcfa69'),
+            'username' => env('DB_USERNAME', 'b3ce9185ce11b8'),
+            'password' => env('DB_PASSWORD', 'c6a83dd1'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
