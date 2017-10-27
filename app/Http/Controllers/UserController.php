edf50c5e563564cf4c62fe5e5539015f1e9c365f
linhnm <linhnm@bap.jp>
2017-10-27 23:38:23 +0700

    fix login_create and logout

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -51,6 +51,7 @@ class UserController extends Controller
             $user->password = bcrypt($user->password);
             $user->email = $request->email;
             $user->level = 1;
+            $user->setRememberToken(self::randomRememberToken());
             $user->save();
             //Them bai 1-2-3 trong user learnt
             self::createFirstThreeLessons($user);
@@ -173,8 +174,7 @@ class UserController extends Controller
             Session::put('userLogin', Auth::user());
             return response()->json([
                 'status' => 'Success',
-//                'token' => Auth::user()->getRememberToken()
-                'token' => Session::get('userLogin')
+                'token' => Auth::user()->getRememberToken()
             ]);
         }
         return response()->json([
@@ -182,11 +182,22 @@ class UserController extends Controller
         ]);
     }
     public function logout(Request $request){
-        return $request->url('/logout');
-        $res = [
-          'status' => 'success',
-          'message' => 'Logout successfully'
-        ];
+        $remember_token = $request->remember_token;
+        $user = User::where('remember_token','=',$remember_token)
+            ->first();
+        if(!$user){
+            $res = [
+                'status' => 'fail',
+                'message' => 'Can not find User'
+            ];
+        }else {
+            $user->setRememberToken(self::randomRememberToken());
+            $user->save();
+            $res = [
+                'status' => 'success',
+                'message' => 'Logout successfully'
+            ];
+        }
         return response()->json($res);
     }
     public static function createFirstThreeLessons($user){
@@ -207,5 +218,18 @@ class UserController extends Controller
         $userLearnt3->IdLesson = 3;
         $userLearnt3->LessonPoint = 0;
         $userLearnt3->save();
+    }
+    public static function randomRememberToken(){
+        $arrCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m'
+                        ,'n','o','p','q','r','s','t','u','v','w','x','y','z'
+                        ,'A','B','C','D','E','F','G','H','I','J','K','L','M'
+                        ,'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+                        ,'0','1','2','3','4','5','6','7','8','9'];
+        $remember_token = '';
+        for ($i = 1 ; $i <= 60; $i++){
+            $index = rand(0,count($arrCharacters)-1);
+            $remember_token = $remember_token .''.$arrCharacters[$index];
+        }
+        return $remember_token;
     }
 }
